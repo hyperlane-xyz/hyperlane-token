ba7ad4dba064396a4e7847d03cb379d0132352b5
yorhodes <yorkerhodesiv@gmail.com>
2022-04-12 10:21:08 -0700

    Prefix function parameters with underscore

--- contracts/AbcToken.sol
+++ contracts/AbcToken.sol
@@ -8,34 +8,34 @@ import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/
 contract AbcToken is Router, ERC20Upgradeable {
     function initialize(
         address _xAppConnectionManager,
-        uint256 totalSupply,
-        string memory name,
-        string memory symbol
+        uint256 _totalSupply,
+        string memory _name,
+        string memory _symbol
     ) external initializer {
         __Router_initialize(_xAppConnectionManager);
-        __ERC20_init(name, symbol);
-        _mint(msg.sender, totalSupply);
+        __ERC20_init(_name, _symbol);
+        _mint(msg.sender, _totalSupply);
     }
 
     // Burns `amount` of tokens from `msg.sender` on the origin chain and dispatches
     // message to the `destination` chain to mint `amount` of tokens to `recipient`.
     function transferRemote(
-        uint32 destination,
-        address recipient,
-        uint256 amount
+        uint32 _destination,
+        address _recipient,
+        uint256 _amount
     ) external {
-        _burn(msg.sender, amount);
-        _dispatchToRemoteRouter(destination, abi.encode(recipient, amount));
+        _burn(msg.sender, _amount);
+        _dispatchToRemoteRouter(_destination, abi.encode(_recipient, _amount));
     }
 
     // Mints `amount` of tokens to `recipient` when router receives transfer `message`.
     function _handle(
         uint32,
         bytes32,
-        bytes memory message
+        bytes memory _message
     ) internal override {
         (address recipient, uint256 amount) = abi.decode(
-            message,
+            _message,
             (address, uint256)
         );
         _mint(recipient, amount);
