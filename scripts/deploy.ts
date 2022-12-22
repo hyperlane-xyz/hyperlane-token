a294fdf5ea498a018178fd90744c9c09276d9686
Yorke Rhodes <yorke@hyperlane.xyz>
2022-12-21 23:34:49 -0500

    Make ism optional

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,4 @@
-import { ethers, Wallet } from 'ethers';
+import { Wallet, ethers } from 'ethers';
 
 import {
   Chains,
@@ -27,31 +27,35 @@ const connectionConfigs = {
 async function deployNFTWrapper() {
   console.info('Getting signer');
   const signer = new Wallet(
-    'pkey',
+    '3ed2c141ec02887887e94c1fbe5647fe5741c038deca248afbaaccf2c27d9258',
   );
 
   const multiProvider = new MultiProvider(connectionConfigs);
-  multiProvider.rotateSigner(signer)
+  multiProvider.rotateSigner(signer);
   const core = HyperlaneCore.fromEnvironment('testnet2', multiProvider);
 
-  const config = objMap(connectionConfigs, (chain, c) => ({
-    type: TokenType.synthetic,
-      name: 'Dai',
-      symbol: 'DAI',
-      totalSupply: 0,
-      owner: signer.address,
-      mailbox: '0x1d3aAC239538e6F1831C8708803e61A9EA299Eec',
-      interchainGasPaymaster: core.getContracts(chain)
-        .interchainGasPaymaster.address,
-  } as TokenConfig & RouterConfig))
+  const config = objMap(
+    connectionConfigs,
+    (chain, _) =>
+      ({
+        type: TokenType.synthetic,
+        name: 'Dai',
+        symbol: 'DAI',
+        totalSupply: 0,
+        owner: signer.address,
+        mailbox: '0x1d3aAC239538e6F1831C8708803e61A9EA299Eec',
+        interchainGasPaymaster:
+          core.getContracts(chain).interchainGasPaymaster.address,
+      } as TokenConfig & RouterConfig),
+  );
   config.goerli = {
-      type: TokenType.collateral,
-      token: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
-      owner: signer.address,
-      mailbox: '0x1d3aAC239538e6F1831C8708803e61A9EA299Eec',
-      interchainGasPaymaster: core.getContracts(Chains.goerli)
-        .interchainGasPaymaster.address,
-  } as TokenConfig & RouterConfig
+    type: TokenType.collateral,
+    token: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
+    owner: signer.address,
+    mailbox: '0x1d3aAC239538e6F1831C8708803e61A9EA299Eec',
+    interchainGasPaymaster: core.getContracts(Chains.goerli)
+      .interchainGasPaymaster.address,
+  } as TokenConfig & RouterConfig;
 
   const deployer = new HypERC20Deployer(multiProvider, config, core);
 
