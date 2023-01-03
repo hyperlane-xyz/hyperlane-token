ee37b4673bfa396a7ccd91fe7662441805d0c490
nambrot <nambrot@googlemail.com>
2023-01-03 16:33:53 -0500

    Add scripts back

---
+++ scripts/deploy.ts
@@ -0,0 +1,43 @@
+import yargs from 'yargs';
+import { ethers } from 'ethers';
+import path from "path";
+import fs from "fs";
+import {
+  MultiProvider,
+  chainConnectionConfigs,
+  objMap,
+} from '@hyperlane-xyz/sdk';
+import { HypERC20Deployer } from "../src/deploy";  
+
+// Function that takes 2 CLI arguments, the private key and the token config file
+async function deployTradeRoute() {
+  const argv = await yargs
+  .option('private-key', {
+    type: 'string',
+    describe: 'Private key for signing transactions',
+    demandOption: true,
+  })
+  .option('config', {
+    type: 'string',
+    describe: 'Path to JSON config file',
+    demandOption: true,
+  })
+  .argv;
+
+  const signer = new ethers.Wallet(argv['private-key']);
+    const config = JSON.parse(fs.readFileSync(path.resolve(argv.config), "utf-8"))
+    const multiProvider = new MultiProvider(
+      objMap(chainConnectionConfigs, (_chain, conf) => ({
+        ...conf,
+        signer: signer.connect(conf.provider),
+      })),
+    );
+
+    const deployer = new HypERC20Deployer(multiProvider, config, undefined);
+    await deployer.deploy();
+
+    console.log('Deployment successful. Deployed contracts:')
+    console.log(deployer.deployedContracts)
+}
+
+deployTradeRoute().then(console.log).catch(console.error)
